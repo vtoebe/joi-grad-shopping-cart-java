0c8cdac2aaaa234fa7653a211ff8f645bd71573b
Vitória Toebe <vitoriatoebe@gmail.com>
2022-01-13 19:15:02 -0300

    feat: add discount & discount pairs
    • added HashMap discountPairs (key : value)
    • feat(setDiscountPairs): populates HashMap with productCode and referring discount
    • feat(setDiscount): calculates discount based on the product code matched with the discount pair

--- src/main/java/com/thoughtworks/codepairing/model/Product.java
+++ src/main/java/com/thoughtworks/codepairing/model/Product.java
@@ -1,25 +1,40 @@
 package com.thoughtworks.codepairing.model;
 
-public class Product {
+import java.util.*;
+
+public class Product{
     private final double price;
     private final String productCode;
     private final String name;
+    private final int discount;
+    static final HashMap<String, Integer> discountPairs = new HashMap<>();
+
 
     public Product(double price, String productCode, String name) {
+        setDiscountPairs();
         this.price = price;
         this.productCode = productCode;
         this.name = name;
+        this.discount = setDiscount();
     }
 
-    public double getPrice() {
-        return price;
+    public double getPrice() { return price; }
+    public String getName() { return name; }
+    public int getDiscount() { return discount; }
+
+    private void setDiscountPairs(){
+        discountPairs.put("DIS_10", 10);
+        discountPairs.put("DIS_15", 15);
+        discountPairs.put("DIS_20", 20);
+        discountPairs.put("DIS_30", 30);
     }
 
-    public String getProductCode() {
-        return productCode;
-    }
-
-    public String getName() {
-        return name;
+    public int setDiscount(){
+        for (Map.Entry<String, Integer> pair : discountPairs.entrySet()){
+            if (productCode.startsWith(pair.getKey())){
+                return pair.getValue();
+            }
+        }
+        return 0;
     }
 }
