341308370d869c759c3550dba9721b99004ef555
Vitória Toebe <vitoriatoebe@gmail.com>
2022-01-13 19:26:26 -0300

    refactor: simplify & cleanup
    • initialize List<Products> with values
    • removed redundant .toString

--- src/main/java/com/thoughtworks/codepairing/SampleApp.java
+++ src/main/java/com/thoughtworks/codepairing/SampleApp.java
@@ -5,26 +5,25 @@ import com.thoughtworks.codepairing.model.Product;
 import com.thoughtworks.codepairing.model.ShoppingCart;
 import com.thoughtworks.codepairing.model.Order;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import static java.util.Arrays.asList;
 
 public class SampleApp {
     public static void main(String[] args) {
-        Product product1 = new Product(10.0, "DIS_10_PRODUCT1", "product 1");
-        Product product2 = new Product(20.0, "DIS_10_PRODUCT2", "product 2");
 
-        List<Product> products = new ArrayList<>();
-        products.add(product1);
-        products.add(product2);
+        List<Product> products = asList(
+                new Product(10.0, "DIS_10_PRODUCT1", "product 1"),
+                new Product(20.0, "DIS_10_PRODUCT2",  "product 2"),
+                new Product(30.0, "DIS_10_PRODUCT3", "product 3")
+        );
 
         Customer customer = new Customer("A Customer");
 
         ShoppingCart shoppingCart = new ShoppingCart(customer, products);
-        Product product3 = new Product(30.0, "DIS_10_PRODUCT3", "product 3");
-        shoppingCart.addProduct(product3);
-        System.out.println(shoppingCart.toString());
+        System.out.println(shoppingCart);
 
         Order order = shoppingCart.checkout();
-        System.out.println(order.toString());
+        System.out.println(order);
     }
 }
